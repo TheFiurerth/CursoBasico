3b1beb8e6332982cfde2392560c87a5566a2a81a
Danielk10 <danielpdiamon@gmail.com>
2025-02-24 19:58:24 -0400

    Configuracion del proyecto

---
+++ app/src/main/java/com/leonel/cursobasico/MainActivity.java
@@ -0,0 +1,27 @@
+
+package com.leonel.cursobasico;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.leonel.cursobasico.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Inflate and get instance of binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        // set content view to binding's root
+        setContentView(binding.getRoot());
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.binding = null;
+    }
+}
